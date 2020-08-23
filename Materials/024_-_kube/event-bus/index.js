49cc2045597382f6b011f372d097dfbc9a219f7a
arunabharjun <sunnyarj114@gmail.com>
2020-08-24 04:40:55 +0530

    Added kubernetes service : type => Cluster IP for inter POD communication

--- Materials/024_-_kube/event-bus/index.js
+++ Materials/024_-_kube/event-bus/index.js
@@ -8,20 +8,20 @@ app.use(bodyParser.json());
 const events = [];
 
 app.post('/events', (req, res) => {
-	const event = req.body;
+  const event = req.body;
 
-	events.push(event);
+  events.push(event);
 
-	axios.post('http://posts-clusterip-srv:4000/events', event);
-	axios.post('http://comments-srv:4001/events', event);
-	axios.post('http://query-srv:4002/events', event);
-	axios.post('http://moderation-srv:4003/events', event);
+  axios.post('http://posts-clusterip-srv:4000/events', event);
+  axios.post('http://comments-srv:4001/events', event);
+  axios.post('http://query-srv:4002/events', event);
+  axios.post('http://moderation-srv:4003/events', event);
 
-	res.send({ status: 'OK' });
+  res.send({ status: 'OK' });
 });
 
 app.get('/events', (req, res) => {
-	res.send(events);
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -11,7 +11,7 @@ const handleEvent = (type, data) => {
 			? 'rejected'
 			: 'approved';
 
-		axios.post('http://localhost:4005/events', {
+		axios.post('http://event-bus-srv:4005/events', {
 			type: 'CommentModerated',
 			data: {
 				id: data.id,
@@ -33,7 +33,7 @@ app.post('/events', async (req, res) => {
 
 app.listen(4003, async () => {
 	console.log('[Moderation] Listening on 4003');
-	const res = await axios.get('http://localhost:4005/events');
+	const res = await axios.get('http://event-bus-srv:4005/events');
 
 	for (let event of res.data) {
 		console.log('Processing event:', event.type);
