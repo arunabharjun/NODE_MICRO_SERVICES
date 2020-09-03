b05d3aafd2e29b641aa0d4c2e343c40f8eb80609
arunabharjun <sunnyarj114@gmail.com>
2020-09-03 07:45:18 +0530

    Configured kubernetes cluster, did skaffolding and setup ingress-nginx server

--- ticketing_proj/auth/src/index.ts
+++ ticketing_proj/auth/src/index.ts
@@ -1,9 +1,17 @@
+// index.ts
+
 import express from 'express';
 import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+	res.json({
+		user: 'Arunabh Arjun '
+	});
+});
+
 app.listen(3000, () => {
-	console.log('[Auth] Listening on PORT 3000');
+	console.log('[Auth] Listening on PORT 3000!');
 });
