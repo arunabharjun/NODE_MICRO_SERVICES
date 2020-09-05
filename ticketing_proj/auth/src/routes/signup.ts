dfabb93829e678956c62f49c308d0a20c7425693
arunabharjun <sunnyarj114@gmail.com>
2020-09-06 05:29:56 +0530

    Added validation for signup

--- ticketing_proj/auth/src/routes/signup.ts
+++ ticketing_proj/auth/src/routes/signup.ts
@@ -1,10 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const router = express.Router();
+import { body } from 'express-validator';
 
-router.post('/api/users/signup', (req, res) => {
-	res.json({
-		User: 'Arunabh Arjun'
-	});
-});
+router.post(
+	'/api/users/signup',
+	[
+		body('email').isEmail().withMessage('Email must be valid'),
+		body('password')
+			.trim()
+			.isLength({
+				min: 4,
+				max: 20
+			})
+			.withMessage('Password must be between 4 & 20 characters')
+	],
+	(req: Request, res: Response) => {
+		res.json({
+			User: 'Arunabh Arjun'
+		});
+	}
+);
 
 export { router as signupRouter };
