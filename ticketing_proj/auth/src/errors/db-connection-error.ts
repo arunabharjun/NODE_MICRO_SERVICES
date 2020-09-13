8e0d3f0ee86cedc2b5a70b69bb17a25b652974f7
arunabharjun <sunnyarj114@gmail.com>
2020-09-14 01:43:46 +0530

    Revisions

--- ticketing_proj/auth/src/errors/db-connection-error.ts
+++ ticketing_proj/auth/src/errors/db-connection-error.ts
@@ -4,7 +4,6 @@ export class DBConnectionError extends Error {
 	reason = 'Error connecting to DB';
 	constructor() {
 		super();
-
 		// Only because we are extending a built in class
 		Object.setPrototypeOf(this, DBConnectionError.prototype);
 	}
