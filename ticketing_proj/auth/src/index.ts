d2a9e8d4d513139f9862b284574b951c2c67d330
arunabharjun <sunnyarj114@gmail.com>
2020-09-18 15:45:24 +0530

    Added user mongoose model

--- ticketing_proj/auth/src/index.ts
+++ ticketing_proj/auth/src/index.ts
@@ -1,6 +1,10 @@
+// libraries
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
+// custom
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
@@ -22,6 +26,21 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-	console.log('[AUTH] Listening on port 3000');
-});
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		});
+		console.log('[AUTH-MONGODB] Connection Successfull!');
+	} catch (err) {
+		console.error(err);
+	}
+
+	app.listen(3000, () => {
+		console.log('[AUTH] Listening on port 3000');
+	});
+};
+
+start();
