3e3bb2257cb90f56659e47ada3d8297580a22402
arunabharjun <sunnyarj114@gmail.com>
2020-09-18 16:33:57 +0530

    Now mongoose and typescript work together

--- ticketing_proj/auth/src/models/user.ts
+++ ticketing_proj/auth/src/models/user.ts
@@ -1,5 +1,39 @@
 import mongoose from 'mongoose';
 
+/**
+ * before defining a mongoose modal
+ * we are going to define an interface in TS
+ * that describes the properties that are 
+ * required to create a new User
+ */
+
+interface UserAttrs {
+	email: string;
+	password: string;
+}
+
+/**
+ * now we ill create another interface that 
+ * describes the properties that a User model has
+ */
+
+interface UserModel extends mongoose.Model<UserDoc> {
+	build(attrs: UserAttrs): UserDoc;
+}
+
+/**
+ * we now create an interface that describes the properties
+ * that a user document has
+ */
+
+interface UserDoc extends mongoose.Document {
+	email: string;
+	password: string;
+}
+
+/**
+ * now we define our model using mongoose
+ */
 const userSchema = new mongoose.Schema({
 	email:
 		{
@@ -13,6 +47,22 @@ const userSchema = new mongoose.Schema({
 		}
 });
 
-const User = mongoose.model('User', userSchema);
+/**
+ * This is how we get a User
+ */
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+	return new User(attrs);
+};
+
+/**
+ * use can use the following build method to get a new user
+ */
+
+// const user = User.build({
+// 	email: 'test@test.com',
+// 	password: 'yayarya'
+// });
 
 export { User };
